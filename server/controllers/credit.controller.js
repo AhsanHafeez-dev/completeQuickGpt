3e3908a443c6ee01490d263c6f7ea1bac8db2290
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-02-22 17:43:11 +0500

    init

--- server/controllers/credit.controller.js
+++ server/controllers/credit.controller.js
@@ -16,7 +16,13 @@ const purchasePlan = asyncHandler(async (req, res) => {
     const plan = plans.filter((plan) => plan._id = planId)[0];
     if (!plan) { throw new ApiError(httpCodes.badRequest, "invalid plam"); }
     
-    const transaction = await prisma.transaction.create({ data: { planId, userId, amount: plan.price, credits: plan.credits, isPaid: false } });
+
+    // when webhooks start working cameback here
+    // const transaction = await prisma.transaction.create({ data: { planId, userId, amount: plan.price, credits: plan.credits, isPaid: false } });
+
+
+    const transaction = await prisma.transaction.create({ data: { planId, userId, amount: plan.price, credits: plan.credits, isPaid: true } });
+    await prisma.user.update({ where: { id: transaction.userId }, data: { credits: { increment: transaction.credits } } });
     const session = await createPaymentGatewayOrder(plan, origin, transaction.id);
     
     return res.status(httpCodes.created).json(new ApiResponse(httpCodes.created, { url: session.url }, "order created succesfully"));
