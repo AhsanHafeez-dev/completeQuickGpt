3c107b5ed7e9f9de422bbea85c86a27af5cefe14
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-03-02 15:20:59 +0500

    optimizations

--- server/prisma/index.js
+++ server/prisma/index.js
@@ -1,6 +1,7 @@
 
 // Pg Bouncer
 import { PrismaPg } from "@prisma/adapter-pg";
+// prisma client
 import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
