25cd89c44a947527e41cc4a3513ecd0cc08d7b68
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-03-02 15:21:06 +0500

    optimizations

--- server/prisma/index.js
+++ server/prisma/index.js
@@ -4,6 +4,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 // prisma client
 import { PrismaClient } from "@prisma/client";
 
+// connection url
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
